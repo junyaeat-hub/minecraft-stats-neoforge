526779ccf8a09db5674992ddb736695b46738696
junyaeat-hub <junyaeat@gmail.com>
2026-08-21 20:19:43 +0200

    Create StatsHudMod.java

---
+++ src/main/java/com/example/statshud/StatsHudMod.java
@@ -0,0 +1,11 @@
+package com.example.statshud;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(StatsHudMod.MODID)
+public class StatsHudMod {
+    public static final String MODID = "statshud";
+
+    public StatsHudMod() {
+    }
+}
